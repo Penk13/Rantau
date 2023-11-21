e571b38bcffe6b913aadc7d2a36897ab424ca2ab
Reinhart2003 <deoxyrheinhart@gmail.com>
2023-11-21 10:04:20 +0700

    TopDownScript

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TopDownMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+    }
+}
