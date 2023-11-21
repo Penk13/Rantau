5081536fcaf5d5dfc8af6db6ce2f915c0e2efdc4
Penk13 <natanaelsteven109@gmail.com>
2023-11-21 11:21:30 +0700

    flip character

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class TopDownMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     Vector2 movement;
+    bool facingRight = true;
 
 
     // Update is called once per frame
@@ -16,10 +15,25 @@ public class TopDownMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        float moveInputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if(moveInputHorizontal < 0 && facingRight){
+            flip();
+        }
+        if(moveInputHorizontal > 0 && !facingRight){
+            flip();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 }
