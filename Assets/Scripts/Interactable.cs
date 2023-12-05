69ea20e5b3d3cb171b0e2f07d2bc29bf968662f6
Penk13 <natanaelsteven109@gmail.com>
2023-12-05 10:45:26 +0700

    add interaction popup

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class Interactable : Collideable
 {
     protected bool z_Interacted = false;
+    public TextMeshProUGUI interact_text;
+    public string target_scene;
+
+    protected override void Start()
+    {
+        base.Start();
+        interact_text.gameObject.SetActive(false);
+    }
+
     protected override void OnCollided(GameObject collidedObject) {
         if (Input.GetKey(KeyCode.E))
         {
@@ -17,12 +29,29 @@ public class Interactable : Collideable
         if (!z_Interacted)
         {
             z_Interacted = true;
-            Debug.Log("INTERACT WITH " + name);
-            SceneManager.LoadScene(name);
+            Debug.Log("INTERACT WITH " + target_scene);
+            SceneManager.LoadScene(target_scene);
         }
         else 
         {
 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("HEY");
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            interact_text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            interact_text.gameObject.SetActive(false);
         }
     }
 }
