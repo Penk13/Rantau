d0729ec78561086de8e23eca6e553da0328c9604
Penk13 <natanaelsteven109@gmail.com>
2024-01-15 17:45:46 +0700

    add farming minigame

---
+++ Assets/Scripts/Minigames/Farming/Collectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player player = other.GetComponent<Player>();
+
+        if (player)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
