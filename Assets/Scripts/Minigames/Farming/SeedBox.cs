1cfcd0fb970fc0d296a1c202cbf976f911938ed6
Penk13 <natanaelsteven109@gmail.com>
2024-01-16 14:35:58 +0700

    plant seed

---
+++ Assets/Scripts/Minigames/Farming/SeedBox.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class SeedBox : Collideable
+{
+    protected bool z_Interacted = false;
+    public TextMeshProUGUI interact_text;
+    public GameObject seed;
+
+    protected override void Start()
+    {
+        base.Start();
+        interact_text.gameObject.SetActive(false);
+    }
+
+    protected override void OnCollided(GameObject collidedObject) {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (collidedObject.tag == "Player")
+            {
+                OnInteract();
+            }
+        }
+    }
+
+    protected virtual void OnInteract(){
+        if (!z_Interacted)
+        {
+            z_Interacted = true;
+
+            Vector3 centerPosition = transform.position;
+            Instantiate(seed, centerPosition, Quaternion.identity);
+        }
+        else 
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            interact_text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            interact_text.gameObject.SetActive(false);
+        }
+    }
+}
